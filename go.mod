e2bd1bada9deb6e1e4fbba0d9f2d0a8d28238077
Pavel @grbit Griaznov <grbitt@gmail.com>
2023-04-17 22:01:03 +0200

    refactoring

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/esiqveland/notify v0.11.0
 	github.com/godbus/dbus/v5 v5.0.5
 )
+
+require golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2
--- main.go
+++ main.go
@@ -14,20 +14,58 @@ import (
 
 	"github.com/esiqveland/notify"
 	"github.com/godbus/dbus/v5"
+	"golang.org/x/xerrors"
 )
 
 const (
-	idPath                  = "/tmp/go-br-id"
+	idPath                  = "/.local/.go-brightness-id-123458"
 	brightnessctlDeviceName = "amdgpu_bl0"
 	brightnessIcon          = "/usr/share/icons/Papirus/64x64/apps/display-brightness.svg"
+
+	brightnessCommand = "brightnessctl -d " + brightnessctlDeviceName
 )
+
+func homeID() string {
+	return os.Getenv("HOME") + idPath
+}
 
 var cfg config
 
-type config struct {
-	debug bool
-	inc   bool
-	dec   bool
+func main() {
+	readConfig()
+
+	b, err := readBrightness()
+	if err != nil {
+		log.Fatalf("can't read brightness %+v\n", err)
+	}
+
+	sq := math.Sqrt(float64(b.current))
+	if sq < 1 {
+		sq = 1
+	}
+
+	debugPl("sq: ", sq)
+
+	switch {
+	case cfg.dec:
+		b.set = int(math.Max(float64(b.current)-sq, 0))
+	case cfg.inc:
+		b.set = int(math.Min(math.Max(float64(b.current)+sq, 0), float64(b.max)))
+	}
+
+	debugPl("setting brightness to", b.set, ", which in percents will be", b.willBeInPercents())
+	runShell(brightnessCommand + " s " + strconv.Itoa(b.set))
+
+	nID, err := sentNotification(b)
+	if err != nil {
+		log.Printf("failed to create notification: %+v", err)
+		os.Exit(3)
+	}
+
+	if err = saveID(nID); err != nil {
+		log.Printf("failed to save notification id to %q: %+v", homeID(), err)
+		os.Exit(4)
+	}
 }
 
 func runShell(script string) string {
@@ -57,7 +95,58 @@ func (b *brightness) willBeInPercents() int {
 	return b.set * 100 / b.max
 }
 
-func main() {
+func readBrightness() (b brightness, err error) {
+	b.current, err = strconv.Atoi(runShell((brightnessCommand + " g")))
+	if err != nil {
+		return b, xerrors.Errorf("parsing current brightness %+v\n", err)
+	}
+
+	b.max, err = strconv.Atoi(runShell((brightnessCommand + " m")))
+	if err != nil {
+		return b, xerrors.Errorf("parsing max brightness %+v\n", err)
+	}
+
+	debugPf("brightness: %+v\n", b)
+
+	return b, nil
+}
+
+func sentNotification(b brightness) (uint32, error) {
+	conn, err := dbus.SessionBus()
+	if err != nil {
+		return 0, xerrors.Errorf("failed connecting to dbus: %w", err)
+	}
+
+	notifier, err := notify.New(conn)
+	if err != nil {
+		return 0, xerrors.Errorf("failed creating notifier: %w", err)
+	}
+
+	nID, err := notifier.SendNotification(notify.Notification{
+		AppName:       "go-brightness",
+		ReplacesID:    getID(),
+		AppIcon:       brightnessIcon,
+		Body:          fmt.Sprintf("Brightness set to: %d%%", b.willBeInPercents()),
+		Summary:       fmt.Sprintf("Brightness set to: %d%%", b.willBeInPercents()),
+		Hints:         map[string]dbus.Variant{"value": dbus.MakeVariant(b.willBeInPercents())},
+		ExpireTimeout: time.Second * 2,
+	})
+	if err != nil {
+		return 0, xerrors.Errorf("failed to send notification: %w", err)
+	}
+
+	debugPl("notification id =", nID)
+
+	return nID, nil
+}
+
+type config struct {
+	debug bool
+	inc   bool
+	dec   bool
+}
+
+func readConfig() {
 	for _, a := range os.Args {
 		switch a {
 		case "-d":
@@ -74,94 +163,15 @@ func main() {
 			cfg.dec = true
 		}
 	}
-
-	xb := "brightnessctl -d " + brightnessctlDeviceName
-
-	var (
-		b   brightness
-		err error
-	)
-
-	b.current, err = strconv.Atoi(runShell((xb + " g")))
-	if err != nil {
-		log.Fatalf("can't parse current brightness %+v\n", err)
-	}
-
-	b.max, err = strconv.Atoi(runShell((xb + " m")))
-	if err != nil {
-		log.Fatalf("can't parse max brightness %+v\n", err)
-	}
-
-	debugPf("brightness: %+v\n", b)
-
-	sq := math.Sqrt(float64(b.current))
-	if sq < 1 {
-		sq = 1
-	}
-
-	debugPl(sq)
-
-	switch {
-	case cfg.dec:
-		b.set = int(math.Max(float64(b.current)-sq, 0))
-	case cfg.inc:
-		b.set = int(math.Min(math.Max(float64(b.current)+sq, 0), float64(b.max)))
-	}
-
-	debugPl("will be in percents", b.willBeInPercents())
-	debugPl("will set to", b.set)
-
-	runShell(xb + " s " + strconv.Itoa(b.set))
-
-	notifier, err := newNotifier()
-	if err != nil {
-		log.Printf("failed to created notifier: %+v", err)
-		os.Exit(3)
-	}
-
-	nID, err := notifier.SendNotification(notify.Notification{
-		AppName:       "go-brightness",
-		ReplacesID:    getID(),
-		AppIcon:       brightnessIcon,
-		Body:          fmt.Sprintf("Brightness set to: %d%%", b.willBeInPercents()),
-		Hints:         map[string]dbus.Variant{"value": dbus.MakeVariant(b.willBeInPercents())},
-		ExpireTimeout: time.Second,
-	})
-	// n.set_hint("value", now100) # only hardware brightness
-	if err != nil {
-		log.Printf("failed to created notification: %+v", err)
-		os.Exit(3)
-	}
-
-	debugPl("notification id =", nID)
-
-	if err := saveID(nID); err != nil {
-		log.Printf("failed to save notification id to %q: %+v", idPath, err)
-		os.Exit(4)
-	}
-}
-
-func newNotifier() (notify.Notifier, error) {
-	conn, err := dbus.SessionBus()
-	if err != nil {
-		return nil, fmt.Errorf("failed connecting to dbus: %w", err)
-	}
-
-	notifier, err := notify.New(conn)
-	if err != nil {
-		return nil, fmt.Errorf("failed creating notifier: %w", err)
-	}
-
-	return notifier, nil
 }
 
 func getID() uint32 {
-	if _, err := os.Stat(idPath); errors.Is(err, os.ErrNotExist) {
+	if _, err := os.Stat(homeID()); errors.Is(err, os.ErrNotExist) {
 	} else if err != nil {
 		return 0
 	}
 
-	bb, err := os.ReadFile(idPath)
+	bb, err := os.ReadFile(homeID())
 	if err != nil {
 		return 0
 	}
@@ -175,7 +185,7 @@ func getID() uint32 {
 }
 
 func saveID(id uint32) error {
-	return os.WriteFile(idPath, []byte(strconv.Itoa(int(id))), 0o777)
+	return os.WriteFile(homeID(), []byte(strconv.Itoa(int(id))), 0o777)
 }
 
 func debugPf(format string, a ...interface{}) {
